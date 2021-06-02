56402c0373c3f624648c409a56dcb94f71a47bf7
christianwarmuth <christianwarmuth@users.noreply.github.com>
2021-06-02 20:07:40 +0000

    deploy: christianwarmuth/christianwarmuth.de@72fb1663de1418f4ae35c8cd87a9d944b14518cb

---
+++ js/index.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+  
+    var $randomnbr = $('.nbr');
+    var $timer= 10;
+    var $it;
+    var $data = 0;
+    var index;
+    var change;
+    var letters = ["C", "h", "r", "i", "s", "t", "i", "a", "n", " ", "W", "a", "r", "m", "u", "t", "h"];
+    
+    $randomnbr.each(function(){
+        
+      change = Math.round(Math.random()*100);
+      $(this).attr('data-change', change);
+      
+    });
+    
+    function random(){
+      return Math.round(Math.random()*9);
+    };
+    
+    function select(){
+      return Math.round(Math.random()*$randomnbr.length+1);
+    };
+    
+    function value(){
+      $('.nbr:nth-child('+select()+')').html(''+random()+'');
+      $('.nbr:nth-child('+select()+')').attr('data-number', $data);
+      $data++;
+      
+      $randomnbr.each(function(){
+          if(parseInt($(this).attr('data-number')) > parseInt($(this).attr('data-change'))){
+            index = $('.ltr').index(this);
+            $(this).html(letters[index]);
+            $(this).removeClass('nbr');
+          }
+      });
+      
+    };
+    
+    $it = setInterval(value, $timer);
+      
+  });
